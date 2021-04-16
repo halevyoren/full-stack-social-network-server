a6dbf6ab40eb431cfa21bb2e8f68524bac6ca3c3
Oren <halevyoren@gmail.com>
2021-04-16 17:02:45 +0300

    Connecting to MongoDB

---
+++ config/db.js
@@ -0,0 +1,25 @@
+// in this file is the mongoDB connection
+
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI'); //getting the mongoURI from the default.json file
+
+//connecting to mongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log('MongoDB Connected');
+  } catch (error) {
+    //logging the error
+    console.log(error);
+
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
