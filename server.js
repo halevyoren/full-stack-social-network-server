1e8fde10a72a6a93ab4bd47ac74449a5e368a8d3
Oren <halevyoren@gmail.com>
2021-04-16 18:03:23 +0300

    adding validation to user register params

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect to database
 connectDB();
 
+//initialize middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running.'));
 
 //define routes
