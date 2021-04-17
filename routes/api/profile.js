e4493fad3c74aa0087e970308e35b9f0d2aa6e47
Oren <halevyoren@gmail.com>
2021-04-17 16:13:07 +0300

    added get github repos by username

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const auth = require('../../middleware/auth');
@@ -220,7 +222,7 @@ router.put(
       res.json(profile);
     } catch (error) {
       console.error(error.message);
-      res.status(500).send('Server error.');
+      res.status(500).send('Server error');
     }
   }
 );
@@ -266,7 +268,7 @@ router.put('/experience/:exp_id', auth, async (req, res) => {
     res.json(profile);
   } catch (error) {
     console.error(error.message);
-    res.status(500).send('Server error.');
+    res.status(500).send('Server error');
   }
 });
 
@@ -289,7 +291,7 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     res.json(profile);
   } catch (error) {
     console.error(error.message);
-    res.status(500).send('Server error.');
+    res.status(500).send('Server error');
   }
 });
 
@@ -347,7 +349,7 @@ router.put(
       res.json(profile);
     } catch (error) {
       console.error(error.message);
-      res.status(500).send('Server error.');
+      res.status(500).send('Server error');
     }
   }
 );
@@ -393,7 +395,7 @@ router.put('/education/:edu_id', auth, async (req, res) => {
     res.json(profile);
   } catch (error) {
     console.error(error.message);
-    res.status(500).send('Server error.');
+    res.status(500).send('Server error');
   }
 });
 
@@ -416,7 +418,38 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     res.json(profile);
   } catch (error) {
     console.error(error.message);
-    res.status(500).send('Server error.');
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from github
+// @access  Public
+router.get('/github/:username', (req, res) => {
+  try {
+    //getting the repos (5) of the user in acesnding order from github
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
   }
 });
 
